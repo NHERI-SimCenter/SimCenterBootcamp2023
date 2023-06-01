cd9a713563b369a0d8f8535493922929a14b768f
fmckenna <fmckenna@berkeley.edu>
2023-06-01 11:06:15 -0700

    fmk - removing C exercies

--- code/c/ExerciseDay2/ex2-1/stresstransform.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRESSTRANSFORM_H
-#define STRESSTRANSFORM_H
-
-void StressTransform(double sigx, double sigy, double tau,
-		    double *sigxR, double *sigyR, double *tauR, 
-		    double theta);
-
-#endif
-
