d7678368a6b01cb7894dafb45cc2ef94f721ed90
fmckenna <fmckenna@berkeley.edu>
2023-06-05 14:01:53 -0700

    fmk - updating pi assignment

--- assignments/C-Day1/pi.c
+++ assignments/C-Day1/pi.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 #include <math.h>
 
+
 int main(int argc, char **argv) {
 
+  if (argc != 2) {
+    printf("Usage: appName numSteps\n");
+    exit(-1);
+  }
+
+  int numSteps = atoi(argv[1]);
+  
   double pi   = 0;
   
   printf("PI = %16.14f, diff(%16.14f)\n",pi, fabs(pi-M_PI));
